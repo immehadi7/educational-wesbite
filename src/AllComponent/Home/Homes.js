35854e07ced2761b44847bdc7d81a592c8581a19
immehadi7 <mehedy.adi7@gmail.com>
2021-10-04 06:06:29 +0600

    learn homeshow done

--- src/AllComponent/Home/Homes.js
+++ src/AllComponent/Home/Homes.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { Row } from 'react-bootstrap';
 import Banner from '../../Banner/Banner';
 import Package from '../Package/Package';
 import './home.css'
 
 const Homes = () => {
-    const [homeCourse , setHomeCourse] = useState([])
+    const [homeCourses , setHomeCourse] = useState([])
     useEffect(()=>{
-        let url =  ` https://immehadi7.github.io/jsonapi/homejsonShow.json` ;
+        let url =  `https://immehadi7.github.io/jsonapi/packageJsonapi.json` ;
             fetch(url)
                 .then(res=>res.json())
-                    .then(data=> console.log(data))
+                    .then(data=> setHomeCourse(data))
 
     } ,[])
 
     return (
         <div>
             <Banner></Banner>
-            <Package></Package>
+
+            <div>
+                <h1>learn our course</h1>
+            <Row xs={1} md={3} className="g-4">
+            {
+                    homeCourses.map(homeCourse => <Package
+                        key={homeCourse.id}
+                        homeCourse={homeCourse} 
+                    ></Package> )
+                }
+            </Row>
+            </div>
+          
+            
+
+            
+           
         </div>
     );
 };
--- src/AllComponent/Package/Package.js
+++ src/AllComponent/Package/Package.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Card, CardGroup, Col } from 'react-bootstrap';
 
-const Package = () => {
+const Package = (props) => {
+    let {id, title , description , price , image} = props.homeCourse
+        console.log(props.homeCourse)
 
     return (
-        <div>
-            <h1>Learn</h1>            
+        <div> 
+           
+            <Col>
+  <Card>
+    <Card.Img variant="top" src={image} />
+    <Card.Body>
+      <Card.Title> {title} </Card.Title>
+      <h3>Price: {price} </h3>
+      <Card.Text>
+       {description}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+
+  
+</Col>    
+
+
+
+
+
         </div>
     );
 };
